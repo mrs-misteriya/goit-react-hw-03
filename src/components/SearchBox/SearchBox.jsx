2d6a7c43520cc52c90e8ecf4c016a4dd0beaa066
mrs_misteriya <nata.nedostupnaya@mail.com>
2024-04-15 00:42:44 +0300

    adds some css

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,12 +2,13 @@ import css from './SearchBox.module.css'
 
 export default function SearchBox({ value, onSearch}) {
     return (
-        <div className={css.searchbox}> 
-          <p>Find contact by name</p>
-          <input type="text" 
-          value={value}  
-          onChange={(e) => onSearch(e.target.value)}/>
-        </div>
-        
-    )
+      <div className={css.searchbox}>
+        <p className={css.searchfield}>Find contact by name</p>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => onSearch(e.target.value)}
+        />
+      </div>
+    );
 }
