f5aa38ac3ea2a934cb4aaeb519f24962bb18d5fa
mrs_misteriya <nata.nedostupnaya@mail.com>
2024-04-14 22:48:54 +0300

    solves tasks

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,13 @@
+import css from './SearchBox.module.css'
+
+export default function SearchBox({ value, onSearch}) {
+    return (
+        <div className={css.searchbox}> 
+          <p>Find contact by name</p>
+          <input type="text" 
+          value={value}  
+          onChange={(e) => onSearch(e.target.value)}/>
+        </div>
+        
+    )
+}
